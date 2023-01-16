1c4ef877656fb22945671e3a81a84c580c07144d
natalia.cavalcante <ncx.xavier@gmail.com>
2023-01-16 09:28:07 -0400

    fix service

--- src/business/Villains/index.ts
+++ src/business/Villains/index.ts
@@ -3,14 +3,9 @@ import { ICharacters } from '../../interfaces/ICharacters';
 import { ErrosHttp } from "../../controller/errorHttp/errorsHttp";
 import { VillainsController } from "../../controller/villainsController";
 export class VillainsBusiness{
-    static validateHeroName(name: string){
-        if(name === '' || name === null || name === undefined || typeof name !== 'string'){
-            throw new Error ("invalid-name")
-        }
-    }
     public async getVillains(req: Request,res:Response){
         const controller = new VillainsController();
-        const villains = await controller.getVillians()
+        const villains:ICharacters[] = await controller.getVillians()
         res.status(200).json(villains)
         try{
         } catch(err:any){
